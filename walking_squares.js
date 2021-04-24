13a631cfa24d541d624dd3f425fc86c61eee5d62
Deathlymad <meyerphilip96@googlemail.com>
2021-04-24 13:11:02 +0200

    added rotation conditions

--- walking_squares.js
+++ walking_squares.js
@@ -5,6 +5,13 @@ function initTileAssets() {
     //TODO init array for tiles. Could be optimized out
 }
 
+export let Transformation = {
+    TOP_LEFT: 0,
+    TOP_RIGHT: 1, //rotate +90 deg
+    BOTTOM_LEFT: 2 //rotate 180 deg
+    BOTTOM_RIGHT: 1, //rotate -90 deg
+}
+
 function computeSquareMap(scanlineArr, width, height) {
     
     let side_offset = floor(width / 2) //offset cube objects so that they start at the middle
@@ -22,6 +29,16 @@ function computeSquareMap(scanlineArr, width, height) {
                 case 0:       //open space
                     break;
                 case 1:       //corner
+                    let transform = null;
+                    if (tl == 1)
+                        transform = Transformation.TOP_LEFT
+                    else if (tr == 1)
+                        transform = Transformation.TOP_RIGHT
+                    else if (bl == 1)
+                        transform = Transformation.BOTTOM_LEFT
+                    else if (br == 1)
+                        transform = Transformation.BOTTOM_RIGHT
+                    
                     level.objects.push(new CollidableGameObject("corner.png", vec2.fromValues(w - side_offset, -(h + depth_offset)), vec2.size(10, 10)))
                     break;
                 case 2:       //wall or corridor
@@ -33,6 +50,16 @@ function computeSquareMap(scanlineArr, width, height) {
                     }
                     break;
                 case 3:       //inverted corner
+                    let transform = null;
+                    if (tl == 0)
+                        transform = Transformation.TOP_LEFT
+                    else if (tr == 0)
+                        transform = Transformation.TOP_RIGHT
+                    else if (bl == 0)
+                        transform = Transformation.BOTTOM_LEFT
+                    else if (br == 0)
+                        transform = Transformation.BOTTOM_RIGHT
+                    
                     level.objects.push(new CollidableGameObject("corner.png", vec2.fromValues(w - side_offset, -(h + depth_offset)), vec2.size(10, 10)))
                     break;
                 case 4:       //solid wall
