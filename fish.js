0a36babf26d5de99033a393ef6c588e5747c4f4c
MDeiml <matthias@deiml.net>
2021-04-25 20:29:53 +0200

    Add octopus

--- fish.js
+++ fish.js
@@ -10,8 +10,8 @@ const NUM_FISH = 30;
 const FISH_RADIUS = 10;
 
 const DEPTHS = [
-    { start: 0 * 4, end: 8 * 4, big_fish: "Assets/fish4/anglerfisch_anim.png", small_fish: ["Assets/fish1/koi.png", "Assets/fish1/angelfish.png", "Assets/fish1/salmon.png"], small_fish_angle: [110, 0, 0]},
-    { start: 24 * 4, end: 32 * 4, big_fish: "Assets/fish4/anglerfisch_anim.png", small_fish: ["Assets/fish1/koi.png"], small_fish: [180]}
+    { start: 0 * 4, end: 8 * 4, big_fish: "Assets/fish1/octopus.png", big_fish_frames: 1, flip_big_fish: true, small_fish: ["Assets/fish1/koi.png", "Assets/fish1/angelfish.png", "Assets/fish1/salmon.png"], small_fish_angle: [110, 0, 0]},
+    { start: 24 * 4, end: 32 * 4, big_fish: "Assets/fish4/anglerfisch_anim.png", big_fish_frames: 4, small_fish: ["Assets/fish1/koi.png"], small_fish: [180]}
 ];
 
 let big_fish;
@@ -26,7 +26,7 @@ export function initFish() {
             vec2.floor(pos, pos);
             if (!level.collidables[pos] || level.collidables[pos].length == 0) {
                 vec2.scale(pos, pos, COLLIDABLE_GRID_SIZE);
-                let obj = new AnimatedGameObject(depth.big_fish, pos, vec2.fromValues(3, 2), "big_fish", 4);
+                let obj = new AnimatedGameObject(depth.big_fish, pos, vec2.fromValues(3, 2), "big_fish", depth.big_fish_frames);
                 obj.depth = d;
                 obj.velocity = vec2.fromValues(0, 0);
                 big_fish.push(obj);
@@ -90,10 +90,11 @@ export function updateFish(delta) {
             let velLength = vec2.length(big_fish[d].velocity);
             vec2.scale(big_fish[d].velocity, big_fish[d].velocity, BIG_FISH_SPEED / velLength);
             big_fish[d].flip = big_fish[d].velocity[0] > 0;
+            big_fish[d].flip ^= depth.flip_big_fish;
         } else {
             vec2.set(big_fish[d].velocity, 0, 0);
         }
     }
-    let angler = big_fish[0];
-    level.updateLight(2, [0.3, 1, 0.5], [angler.position[0] - 1.2, angler.position[1] + 0.4],[0, 1], -1.0, 1);
+    let angler = big_fish[1];
+    level.updateLight(2, [0.3, 1, 0.5], [angler.position[0] - (angler.flip ? -1.2 : 1.2), angler.position[1] + 0.4],[0, 1], -1.0, 1);
 }
