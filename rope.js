db4f07f3fbca8642c81a533d3c85216cfa85c1b7
MDeiml <matthias@deiml.net>
2021-04-25 10:53:15 +0200

    Simplest rope physics

--- rope.js
+++ rope.js
@@ -1,15 +1,26 @@
-import {level} from "./state.js";
+import {level, player, updateRegistry} from "./state.js";
 import {GameObject} from "./GameObject.js";
 import {vec2} from "./gl-matrix-min.js";
+
+let ropes = [];
+
+export function updateRopes(delta) {
+    for (let rope of ropes) {
+        rope.update(delta);
+    }
+}
 
 export let Rope = function(asset) {
     this.segments = [];
     this.points = [];
+    this.points_velocity = [];
     this.asset = asset;
+    ropes.push(this);
 }
 
 Rope.prototype.addPoint = function(point) {
     this.points.push(point);
+    this.points_velocity.push(vec2.fromValues(Math.random() * 0.1 - 0.05, Math.random() * 0.1 - 0.05));
     if (this.points.length >= 2) {
         let segment = new GameObject(this.asset, vec2.create(), vec2.fromValues(0.3, 1.05), "rope", vec2.fromValues(1, 1), vec2.fromValues(0, 0), 0);
         this.segments.push(segment);
@@ -27,4 +38,31 @@ Rope.prototype.updateSegment = function(index) {
     this.segments[index].halfSize[1] = ropeDirLength / 2 * 1.05;
     this.segments[index].orientation = angle;
     this.segments[index].setPosition(ropeMid);
+}
+
+Rope.prototype.update = function(delta) {
+    let updateLast = false;
+    for (let i = 0; i < this.points.length; i++) {
+        let updateCurrent = vec2.squaredDistance(this.points[i], player.position) < 15 * 15;
+        if (updateCurrent) {
+            let dir = vec2.create();
+            if (i != 0) {
+                vec2.sub(dir, this.points[i - 1], this.points[i]);
+                let dirLen = vec2.length(dir);
+                vec2.scale(dir, dir, dirLen - 1);
+                vec2.scaleAndAdd(this.points_velocity[i], this.points_velocity[i], dir, delta * 0.1);
+            }
+            if (i != this.points.length - 1) {
+                vec2.sub(dir, this.points[i + 1], this.points[i]);
+                let dirLen = vec2.length(dir);
+                vec2.scale(dir, dir, dirLen - 1);
+                vec2.scaleAndAdd(this.points_velocity[i], this.points_velocity[i], dir, delta * 0.1);
+            }
+            vec2.scaleAndAdd(this.points[i], this.points[i], this.points_velocity[i], delta);
+        }
+        if ((updateLast || updateCurrent) && i != 0) {
+            this.updateSegment(i - 1);
+        }
+        updateLast = updateCurrent;
+    }
 }
