4bc8961ba0bc87944518f70b68879aa9bba03b3e
Deathlymad <meyerphilip96@googlemail.com>
2021-04-26 13:33:43 +0200

    added resources to tabhle

--- resource.js
+++ resource.js
@@ -2,15 +2,64 @@ import {Texture2D} from "./Sprite.js"
 
 // ls -1 assets/* | xargs printf "%s",\n
 let preload_textures = [
-// "Segments/0010.png",
-// "Segments/0011.png",
-// "Segments/0101_e.png",
-// "Segments/1010_f.png",
-// "Segments/1111.png",
-// "Segments/0111.png",
-"Assets/background.jpg",
-"Assets/fish4/anglerfisch_anim.png",
-"Assets/background_blue.png"
+    //segments
+    "Segments/0010.png",
+    "Segments/0011.png",
+    "Segments/0101_e.png",
+    "Segments/1010_f.png",
+    "Segments/1111.png",
+    "Segments/0111.png",
+
+    //deco
+    "Assets/animationen/qualle_anim.png",
+    "Assets/bubble-alt.png",
+    "Assets/bubble.png",
+    "Assets/luftblase_koralle.png",
+    "Assets/koralle_anim.png",
+    "Assets/alge_anim.png",
+    "Assets/leiche.png",
+    "Assets/rope_g.png",
+    
+    //taucher
+    "Assets/animationen/taucher-animation.png",
+    "Assets/animationen/idle_anim.png", 
+    "Assets/rope.png",
+
+    //fishes
+    "Assets/fish1/pussy_anim.png",
+    "Assets/fish1/koi_anim.png",
+    "Assets/fish1/angelfish.png",
+    "Assets/fish1/salmon.png",
+    "Assets/fish2/shark_anim.png",
+    "Assets/fish2/guppy.png",
+    "Assets/fish2/wels.png",
+    "Assets/fish2/clownfish.png",
+    "Assets/fish4/anglerfisch_anim.png",
+    "Assets/fish3/pirania.png",
+    "Assets/fish3/eel.png",
+    "Assets/fish3/blobfish.png"
+
+    //background
+    "Assets/background_blue.png",
+    "Assets/hintergrund-leer.png",
+    "Assets/hintergrund-boot-leer.png",
+    "Assets/background2.png",
+    
+    //ui
+    "Assets/frame.png",
+    "Assets/tank+barometer.png",
+    "Assets/anzeigepfeil.png",
+    
+    
+    //sounds
+    "Assets/audio/bubble_pop.wav",
+    "Assets/audio/blubbles_breath1.wav",
+    "Assets/audio/blubbles_breath2.wav",
+    "Assets/audio/blubbles_breath3.wav",
+    "Assets/audio/breath1.wav",
+    "Assets/audio/breath2.wav",
+    "Assets/audio/breath3.wav",
+    "Assets/audio/death_short1.wav"
 ]
 
 
